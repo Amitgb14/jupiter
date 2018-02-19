c4aef9bb83e35a8547d4b30a4404adb790cab53c
Amit Ghadge <amitg.b14@gmail.com>
2018-02-19 23:20:26 +0530

    second commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+    )
+from .forms import UserLoginForm
 
-# Create your views here.
+def login_view(request):
+    form = UserLoginForm(request.POST or None)
+    next = request.GET.get('next')
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect("/")
+    return render(request, "sign-in.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+
+@login_required(login_url="/login/")
+def dashboard(request):
+    return render(request, "profile.html")
+
--- jupiter/settings.py
+++ jupiter/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'api',
     'client',
     'certificate',
+    'dashboard',
 ]
 
 REST_FRAMEWORK = {
